9433a49b12f321d7bd7f25e3be93f764979417fa
DAIANE <mdaianesilva1@gmail.com>
2022-05-30 16:22:21 -0300

    Blog Pessoal

--- Spring/blog_pessoal/src/main/java/com/generation/blogpessoal/Repository/PostagemRepository.java
+++ Spring/blog_pessoal/src/main/java/com/generation/blogpessoal/Repository/PostagemRepository.java
@@ -1,5 +1,7 @@
 package com.generation.blogpessoal.Repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,8 @@ import com.generation.blogpessoal.model.Postagem;
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long>{
 
+	public List<Postagem> findAllByTituloContainingIgnoreCase(String titulo);
+
+	
 	
 }
